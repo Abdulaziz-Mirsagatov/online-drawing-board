7139de1e8158e3c508175599b1400d32262fae8c
Abdulaziz-Mirsagatov <mirsagah@mail.uc.edu>
2024-02-24 00:00:59 -0500

    start logging

--- actions/index.ts
+++ actions/index.ts
@@ -5,16 +5,21 @@ import { Board } from "@/types/env";
 import { revalidateTag } from "next/cache";
 
 export const addBoard = async (title: string): Promise<Board> => {
-  const response = await fetch(`${process.env.API_URL}/api/board`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ title }),
-  });
+  try {
+    const response = await fetch(`${process.env.API_URL}/api/board`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title }),
+    });
 
-  revalidateTag("boards");
-  return response.json();
+    revalidateTag("boards");
+    return response.json();
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
 };
 
 export const deleteBoard = async (boardId: string): Promise<void> => {
